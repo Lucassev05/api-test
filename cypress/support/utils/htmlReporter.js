89c5bd8b8b45b503c93a02ee13a9dcf3817424e0
Lucas Evangelista <lucas1994@gmail.com>
2021-07-25 22:07:47 -0300

    fix: htmlReporter

--- cypress/support/utils/htmlReporter.js
+++ cypress/support/utils/htmlReporter.js
@@ -5,23 +5,13 @@ report.generate({
   reportPath: "./cypress/reports/",
   metadata: {
     browser: {
-      name: "chrome",
+      name: "edge",
       version: "60",
     },
-    device: "Local test machine",
+    device: "Máquina Local",
     platform: {
-      name: "ubuntu",
-      version: "16.04",
+      name: "windows",
+      version: "20H2",
     },
-  },
-  customData: {
-    title: "Run info",
-    data: [
-      { label: "Project", value: "Custom project" },
-      { label: "Release", value: "1.2.3" },
-      { label: "Cycle", value: "B11221.34321" },
-      { label: "Execution Start Time", value: "Nov 19th 2017, 02:31 PM EST" },
-      { label: "Execution End Time", value: "Nov 19th 2017, 02:56 PM EST" },
-    ],
   },
 });
